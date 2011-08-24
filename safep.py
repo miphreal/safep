38f3cee710044c0ad662af76e9bebbd5d4ae82ce
Miphreal Adler <miphreal@gmail.com>
2011-08-24 21:48:12 +0300

    remove def preloop... def postloop... they functionality are moved in __init__ and exit

--- safep.py
+++ safep.py
@@ -14,6 +14,8 @@ import cmd
 
 
 class SafeStorage:
+    def __init__(self, db, passwd):
+        pass
     def close(self):
         pass
 
@@ -22,18 +24,17 @@ class SafepCLI(cmd.Cmd):
     prompt = 'safep> '
     intro = 'safep %s'%__version__
 
+    def __init__(self, db, passwd):
+        cmd.Cmd.__init__(self)
+        self.db = SafeStorage(db, passwd)
+
     def do_exit(self, line):
         """
         Exit from safep.
         
         """
-        return True
-
-    def preloop(self):
-        self.db = SafeStorage()
-
-    def postloop(self):
         self.db.close()
+        return True
 
     def do_ls(self, line):
         pass
@@ -54,22 +55,23 @@ class SafepCLI(cmd.Cmd):
 
 def parse_args():
     import optparse
+
     usage = '%prog [-f /path/to/pass] [-p password]'
     parser = optparse.OptionParser(usage=usage)
     parser.add_option('-f', '--file', default='~/.safep', dest='file',
                       help='path to passwords db [default: %default]')
     parser.add_option('-p', '--pass', dest='passwd', help='password for db')
     (options, args) = parser.parse_args()
+
     if not options.passwd:
         print 'password>',
         options.passwd = raw_input()
-    return (options.file, options.passwd)
+        
+    return options.file, options.passwd
 
 
 if __name__ == '__main__':
-
-    print parse_args()
-    SafepCLI().cmdloop()
+    SafepCLI(*parse_args()).cmdloop()
 
 
 

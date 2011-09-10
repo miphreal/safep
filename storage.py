a060f56bf4a307d6e883c4a60e0f580431dc6d94
Miphreal Adler <miphreal@gmail.com>
2011-09-10 22:18:15 +0300

    add few tests

--- storage.py
+++ storage.py
@@ -2,7 +2,7 @@ __author__ = 'miph'
 
 
 import base64
-b64e = base64.encodestring
+b64e = lambda s: base64.encodestring(s).rstrip()
 b64d = base64.decodestring
 
 from functools import partial
@@ -15,13 +15,7 @@ except ImportError:
 
 
 
-
-def half(s):
-    l = len(s)/2
-    return s[:l], s[l:]
-
-
-def cipher(algo, secret):
+def _cipher(algo, secret):
     h = hash_func()
     h.update(secret)
     hash = h.hexdigest()
@@ -33,47 +27,75 @@ def cipher(algo, secret):
         secret = secret[:32]
     return algo.new(secret)
 
-aes = partial(cipher, AES)
-blowfish = partial(cipher, Blowfish)
+aes = partial(_cipher, AES)
+blowfish = partial(_cipher, Blowfish)
 
 
 def pack(data):
-    data = b64e(data).rstrip()
+    data = b64e(data)
     m = len(data) % 16
     return data + (16-m)*'~'
 
 def unpack(data):
     return b64d(data.rstrip('~'))
 
+
 def encode(cipher, data):
-    return cipher.encrypt(pack(data))
+    return b64e(cipher.encrypt(pack(data))).replace('\n','')
 
 def decode(cipher, data):
-    return unpack(cipher.decrypt(data))
+    return unpack(cipher.decrypt(b64d(data)))
 
+
+def parse_record(data):
+    return tuple([b64d(r) for r in data.split(',')])
+
+def build_record(name, user, password, keywords):
+    return b64e(name),b64e(user),b64e(password),b64e(keywords)
+
+# cipher order:
+# save: text -> b64e -> +~~~ -> encrypt -> b64e
+# load: b64d -> decrypt -> rstrip ~~~ -> b64d -> text
+
+class PasswordError(Exception):
+    pass
 
 class SafeStorage:
     #NOTE. record fields: name, user, password, keywords
     def __init__(self, db, passwd):
         self._file_db = db
-        h = hash_func()
-        h.update(passwd)
-        self._hash = h.hexdigest()
-        #open db
-        blf, aes = half(self._hash)
-        cipher = Blowfish.new(blf)
+        #aes and blowfish ciphers
+        self._aes = aes(passwd)
+        self._blowfish = blowfish(passwd)
+
+        self._map = []
         with open(self._file_db,'r') as f:
             for line in f.readlines():
-                pass
+                if line:
+                    try:
+                        self._map.append(
+                            parse_record(
+                                decode(self._aes, line.rstrip())))
+                    except Exception:
+                        raise PasswordError
 
     def close(self):
         pass
 
-    def search(self):
-        pass
+    def search(self, word):
 
-    def add(self):
-        pass
+        return self._map
+
+    def add(self, name, user, password, keywords):
+        password = encode(self._blowfish, password)
+        builded = ','.join(build_record(name, user, password, keywords))
+        encoded = encode(self._aes, builded)
+        self._map.append((name, user, password, keywords))
+        #saving to file
+        with open(self._file_db, 'a') as f:
+            f.write(encoded)
+            f.write('\n')
+
 
     def edit(self):
         pass
@@ -83,3 +105,12 @@ class SafeStorage:
     
     def change_password(self):
         pass
+
+    def _resave(self, password=None):
+        with open(self._file_db,'w') as f:
+            f.truncate()
+            for record in self._map:
+                builded = ','.join(build_record(*record))
+                encoded = encode(self._aes, builded)
+                f.write(encoded)
+                f.write('\n')
---
+++ tests/test_storage.py
@@ -0,0 +1,28 @@
+__author__ = 'miph'
+
+import unittest
+import storage
+
+class MyTestCase(unittest.TestCase):
+    def test_pack_unpack(self):
+        for data in ('some data', 'sd',
+                     'sooooooooooooooooome daaaaaaaaaaaaaataaaaaaaaaaaaaaaaa'):
+            pack_data = storage.pack(data)
+            self.assertEqual(data, storage.unpack(pack_data))
+
+    def test_encode_decode(self):
+        for cipher in (storage.aes, storage.blowfish):
+            c = cipher('pass')
+            for data in ('some data', 'sd',
+                         'sooooooooooooooooome daaaaaaaaaaaaaataaaaaaaaaaaaaaaaa'):
+                encode_data = storage.encode(c, data)
+                self.assertEqual(data, storage.decode(c, encode_data))
+
+    def test_parse_build(self):
+        data = ('record name','miphreal','my pass','kw, test; hi')
+        b = storage.build_record(*data)
+        self.assertTupleEqual(data, storage.parse_record(','.join(b)))
+
+
+if __name__ == '__main__':
+    unittest.main()
